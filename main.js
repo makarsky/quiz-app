0dc984e904523e9660639a51779b2be137ea6fd2
Ihar Makarski <22708426+makarsky@users.noreply.github.com>
2018-05-08 23:58:39 +0300

    Remove unused features

--- main.js
+++ main.js
@@ -59,17 +59,18 @@ function validateForm() {
   // This function deals with validation of the form fields
   var x, y, i, valid = true;
   x = document.getElementsByClassName("tab");
-  y = x[currentTab].getElementsByTagName("input");
-  // A loop that checks every input field in the current tab:
-  for (i = 0; i < y.length; i++) {
-    // If a field is empty...
-    if (y[i].value === "") {
-      // add an "invalid" class to the field:
-      y[i].className += " invalid";
-      // and set the current valid status to false
-      valid = false;
-    }
-  }
+  y = x[currentTab].querySelector("input");
+  
+  // A loop that checks input fields in the current tab:
+  // for (i = 0; i < y.length; i++) {
+  //   // If a field is empty...
+  //   if (y[i].value === "") {
+  //     // add an "invalid" class to the field:
+  //     y[i].className += " invalid";
+  //     // and set the current valid status to false
+  //     valid = false;
+  //   }
+  // }
   // If the valid status is true, mark the step as finished and valid:
   if (valid) {
       document.getElementsByClassName("step")[currentTab].className += " correct";
